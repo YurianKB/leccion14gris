4243835c41e79350abe64f0049ca9a2e09f20962
YurianKB <yuriankb@gmail.com>
2017-03-16 09:48:06 +0000

    ejerciciosleccion14

--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,1 +1,19 @@
-alert("holo");
+function convertirCelsius(farenheit)
+{
+  var numeroFarenheit = document.getElementById('farenheit').value;
+  numeroFarenheit = parseInt(numeroFarenheit);
+  var celsius = document.getElementById('celsius');
+  var resultado = 0;
+  resultado = (numeroFarenheit - 32) * 5/9;
+  celsius.value = resultado;
+}
+
+function convertirFarenheit(farenheit)
+{
+  var numeroCelsius = document.getElementById('celsius').value;
+  numeroCelsius = parseInt(numeroCelsius);
+  var farenheit = document.getElementById('farenheit');
+  var resultado = 0;
+  resultado = (numeroCelsius*9/5)+32;
+  farenheit.value = resultado;
+}
